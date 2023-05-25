cfce919a4ff0bd54f3ee2856a8804c0a8cb44c12
Hoon Won <hoon@lagood.me>
2023-05-26 01:14:16 +0900

    add gamemanger to ham

--- Assets/Scripts/Enemy_temp.cs
+++ Assets/Scripts/Enemy_temp.cs
@@ -1,138 +1,131 @@
 using System.Collections;
 using UnityEngine;
 
-public class Enemy_temp : MonoBehaviour
+public class Enemy_temp : Health
 {
     private GameObject player;
 
+    [Header("Health Property")]
+    [SerializeField]
+    public Vector3 healthBarOffset = new Vector3(0, 2.5f, 0);
+
+    [SerializeField]
+    GameObject healthBarPrefab;
+
+    private HealthBar healthBar;
+
+    [Space(10f)]
     [Header("Move Property")]
     [SerializeField]
-    private GameObject[] movePoints;
+    public GameObject[] movePoints;
     private Vector3[] movePointPos;
-    [SerializeField]
-    private float moveInterval = 10f;
+
     [SerializeField]
     private float minimumMoveInterval = 5f;
+
     [SerializeField]
     private float rotateSpeed = 200f;
-    
-    private float revisedMoveInterval;
-    private float timeSinceLastMove = 0f;
-    private int currentPoint = 0;
-    private bool isMovingNext = true;
 
-    [Space (10f)]
+    [Space(10f)]
     [Header("Attack Property")]
     [SerializeField]
     private GameObject stonePrefab;
-    [SerializeField]
-    private float attackInterval = 10.0f;
 
     [SerializeField]
     private float minimumAttackInterval = 5.0f;
 
-    [SerializeField]
-    private float waitBeforeThrowing = 2.0f;
-
-    [SerializeField]
-    private float waitAfterThrowing = 2.0f;
-
-    private float revisedAttackInterval;
-    private float timeSinceLastAttack = 0f;
-
     private UnityEngine.AI.NavMeshAgent agent;
 
-    private IEnumerator coroutineMove;
-    private IEnumerator coroutineAttack;
+    private Moving moving;
+    private Attacking attacking;
+    private Animator animator;
+    private GameManager gameManager;
 
-    private void Start()
+    protected override void Start()
     {
+        base.Start();
+        gameManager = GameManager.Instance;
+
+        OnDeath += OnEnemyDeath;
+        OnHealthChanged += OnEnemyHealthChanged;
+
+        moving = GetComponent<Moving>();
+        attacking = GetComponent<Attacking>();
+        animator = GetComponent<Animator>();
+
         player = GameObject.Find("Main Camera");
         agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
-        revisedMoveInterval = moveInterval - minimumMoveInterval;
-        revisedAttackInterval = attackInterval - minimumAttackInterval;
+    }
 
-        // Set move positions with random noise
+    public IEnumerator MoveAndAttackLevel1()
+    {
         movePointPos = new Vector3[movePoints.Length];
         for (int i = 0; i < movePoints.Length; i++)
         {
-            movePointPos[i] = movePoints[i].transform.position
-            + new Vector3(Random.Range(-1.0f, 1.0f), 0, Random.Range(-1.0f, 1.0f));
+            movePointPos[i] =
+                movePoints[i].transform.position
+                + new Vector3(Random.Range(-1.0f, 1.0f), 0, Random.Range(-1.0f, 1.0f));
         }
 
-        StartCoroutine(MoveAndAttack());
-    }
+        // move to point1
+        animator.SetBool("isMoving", true);
+        yield return StartCoroutine(moving.MoveToPoint(movePointPos[0]));
+        yield return StartCoroutine(moving.RotateTowards(player.transform.position, rotateSpeed));
+        animator.SetBool("isMoving", false);
+        yield return new WaitForSeconds(minimumMoveInterval + Random.Range(0f, 2f));
 
-    private IEnumerator MoveAndAttack()
-    {
-        while(true)
+        // move to point2
+        animator.SetBool("isMoving", true);
+        yield return StartCoroutine(moving.MoveToPoint(movePointPos[1]));
+        yield return StartCoroutine(moving.RotateTowards(player.transform.position, rotateSpeed));
+        animator.SetBool("isMoving", false);
+        yield return new WaitForSeconds(minimumMoveInterval + Random.Range(0f, 2f));
+
+        // move to point3 (attack position)
+        animator.SetBool("isMoving", true);
+        yield return StartCoroutine(moving.MoveToPoint(movePointPos[2]));
+        yield return StartCoroutine(moving.RotateTowards(player.transform.position, rotateSpeed));
+        animator.SetBool("isMoving", false);
+
+        // attack
+        while (true)
         {
-            if (isMovingNext)
-            {
-                Debug.Log("Moving to next position");
-                coroutineMove = GetComponent<Moving>().MoveToPoint(movePointPos[currentPoint], rotateSpeed);
-                yield return StartCoroutine(coroutineMove);
-                isMovingNext = false;
-            }
-
-            // Move to next position
-            timeSinceLastMove += Time.deltaTime;
-            if ((timeSinceLastMove > minimumMoveInterval) && (currentPoint != movePointPos.Length - 1))
-            {
-                float p = Time.deltaTime / revisedMoveInterval; // probabilty for moving per 1 frame
-                float randomNumber = Random.Range(0f, 1f / p);
-                if (randomNumber > (1f / p) - 1)
-                {
-                    currentPoint += 1;
-                    isMovingNext = true;
-                    timeSinceLastMove = 0f;
-                }
-            }
-
-            // Attack
-            timeSinceLastAttack += Time.deltaTime;
-            if (timeSinceLastAttack > minimumAttackInterval)
-            {
-                float p = Time.deltaTime / revisedAttackInterval; // probabilty for attacking per 1 frame
-                float randomNumber = Random.Range(0f, 1f / p);
-                if (randomNumber > (1f / p - 1))
-                {
-                    Vector3 originPos = agent.transform.position;
-                    Vector3 throwPos =
-                        originPos + new Vector3(Random.Range(-5.0f, 5.0f), 0, Random.Range(-2.0f, 2.0f));
-                    
-                    // Move to throw position
-                    Debug.Log("Start moving to throw");
-                    coroutineMove = GetComponent<Moving>().MoveToPoint(throwPos, rotateSpeed);
-                    yield return StartCoroutine(coroutineMove);
-
-                    // Wait to throw
-                    Debug.Log("Waiting for throwing");
-                    yield return new WaitForSeconds(
-                        Random.Range(waitBeforeThrowing - 1.0f, waitBeforeThrowing + 1.0f));
-
-                    // Throw
-                    Attacking _enemyAttacking = GetComponent<Attacking>();
-                    coroutineAttack = _enemyAttacking.Attack(stonePrefab, Random.Range(30f, 60f));
-                    yield return StartCoroutine(coroutineAttack);
-
-                    // Wait to return
-                    Debug.Log("Waiting for returning");
-                    yield return new WaitForSeconds(
-                        Random.Range(waitAfterThrowing - 1.0f, waitAfterThrowing + 1.0f));
-                    
-                    // Returning to original position
-                    Debug.Log("Start returning to original position");
-                    coroutineMove = GetComponent<Moving>().MoveToPoint(originPos, rotateSpeed);
-                    yield return StartCoroutine(coroutineMove);
-
-                    Debug.Log("Finished throwing");
-                    timeSinceLastAttack = 0f;
-                }
-            }
-
-            yield return null;
+            attacking.Attack(stonePrefab, Random.Range(30f, 60f));
+            yield return new WaitForSeconds(minimumAttackInterval + Random.Range(0f, 3f));
         }
     }
 
+    void OnEnemyDeath()
+    {
+        gameManager.leftEnemyCount -= 1;
+        healthBar.SetHealth(0);
+
+        if (gameManager.leftEnemyCount == 0)
+        {
+            gameManager.currentLevel += 1;
+            gameManager.LevelStart();
+        }
+
+        Destroy(healthBar.gameObject);
+        Destroy(gameObject);
+    }
+
+    void OnEnemyHealthChanged(int currentHealth)
+    {
+        healthBar.SetHealth(currentHealth);
+    }
+
+    public void SetHealthBar()
+    {
+        Canvas canvas = GameObject.Find("Canvas").GetComponent<Canvas>();
+        GameObject healthbarInstance = Instantiate<GameObject>(healthBarPrefab, canvas.transform);
+
+        healthbarInstance.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
+        healthBar = healthbarInstance.GetComponent<HealthBar>();
+
+        healthBar.SetMaxHealth(maxHealth);
+        healthBar.SetHealth(maxHealth);
+        healthBar.target = gameObject.transform;
+        healthBar.offset = healthBarOffset;
+    }
 }
