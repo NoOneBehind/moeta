e2f43848440c4188a3847a8f95d37a25b5fdceb8
YULMU <83626845+yulmu-catsetki@users.noreply.github.com>
2023-06-02 11:36:40 +0900

    add korean font

--- Assets/Scripts/TutorialManager.cs
+++ Assets/Scripts/TutorialManager.cs
@@ -23,8 +23,8 @@ public class TutorialManager : MonoBehaviour
     private void ShowFirstPopUp()
     {
         popUpWindow.SetActive(true);
-        popUpText.text = "first text";
-        //popUpText.text = "자, 내가 돌을 던져줄 테니 다섯 개만 피해 봐!";
+        //popUpText.text = "first text";
+        popUpText.text = "자, 내가 돌을 던져줄 테니 다섯 개만 피해 봐!";
 
         Invoke("ThrowStone", 5f);
     }
@@ -32,8 +32,8 @@ public class TutorialManager : MonoBehaviour
     private void ShowSecondPopUp()
     {
         popUpWindow.SetActive(true);
-        popUpText.text = "second text";
-        //popUpText.text = "아버지: 잘했다. 이제 바위에 던져! 다섯 개를 맞춰보자!";
+        //popUpText.text = "second text";
+        popUpText.text = "잘했다. 이제 바위에 던져! 다섯 개를 맞춰보자!";
 
         canGrabStone = true;
     }
