5d43c1598aa866bd8575e1dc1aa2371b5632624d
frredy99 <frredy99@gmail.com>
2023-06-12 08:05:26 +0900

    WIP: Stone Select UI
    Modify Shield collider

--- Assets/Scripts/StoneSelector.cs
+++ Assets/Scripts/StoneSelector.cs
@@ -1,13 +1,26 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class StoneSelector : MonoBehaviour
 {
+    [SerializeField]
+    private int currentStoneMode;
+    [SerializeField]
+    private GameObject normalStone;
+    [SerializeField]
+    private GameObject boostStone;
+    [SerializeField]
+    private GameObject mommyStone;
+    private List<GameObject> stoneList = new List<GameObject>();
+
+    private XRInteractionManager interactionManager;
     private GameObject rightController;
     private GameObject rayIneteractController;
     private ActionBasedController controller;
+    private XRDirectInteractor directInteractor;
     private XRRayInteractor rayInteractor;
     private XRInteractorLineVisual lineVisual;
 
@@ -16,20 +29,26 @@ public class StoneSelector : MonoBehaviour
 
     void Start()
     {
+        stoneList.Add(normalStone);
+        stoneList.Add(boostStone);
+        stoneList.Add(mommyStone);
+
         rightController = GameObject.Find("RightHand Controller");
         rayIneteractController = GameObject.Find("LeftRayInteractController");
         controller = rightController.GetComponent<ActionBasedController>();
 
+        directInteractor = rightController.GetComponent<XRDirectInteractor>();
         rayInteractor = rayIneteractController.GetComponent<XRRayInteractor>();
         lineVisual = rayIneteractController.GetComponent<XRInteractorLineVisual>();
 
         canvas = gameObject.transform.GetChild(0).gameObject;
         selectUICanvas = canvas.GetComponent<Canvas>();
+        selectUICanvas.worldCamera = Camera.main;
 
-        StartCoroutine(SelectUI());
+        StartCoroutine(SelectUIopen());
     }
 
-    private IEnumerator SelectUI()
+    private IEnumerator SelectUIopen()
     {
         while (true)
         {
@@ -50,8 +69,54 @@ public class StoneSelector : MonoBehaviour
                     rayInteractor.enabled = true;
                     lineVisual.enabled = true;
                 }
+                yield return null;
             }
             yield return null;
+        }
+    }
+
+    public void SelectNormal()
+    {
+        if (currentStoneMode != 0)
+        {
+            GetComponent<SphereCollider>().enabled = false;
+            GameObject switchedStone = Instantiate(
+                stoneList[0],
+                transform.position,
+                transform.rotation
+            );
+            interactionManager.SelectEnter(directInteractor, switchedStone.GetComponent<XRGrabInteractable>());
+            Destroy(gameObject);
+        }
+    }
+
+    public void SelectBoost()
+    {
+        if (currentStoneMode != 1)
+        {
+            GetComponent<SphereCollider>().enabled = false;
+            GameObject switchedStone = Instantiate(
+                stoneList[1],
+                transform.position,
+                transform.rotation
+            );
+            interactionManager.SelectEnter(directInteractor, switchedStone.GetComponent<XRGrabInteractable>());
+            Destroy(gameObject);
+        }
+    }
+
+    public void SelectMommy()
+    {
+        if (currentStoneMode != 2)
+        {
+            GetComponent<SphereCollider>().enabled = false;
+            GameObject switchedStone = Instantiate(
+                stoneList[2],
+                transform.position,
+                transform.rotation
+            );
+            interactionManager.SelectEnter(directInteractor, switchedStone.GetComponent<XRGrabInteractable>());
+            Destroy(gameObject);
         }
     }
 }
